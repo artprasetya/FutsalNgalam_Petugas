2e0fca56f4a009b1d0004f7949180db60ba4745e
PRAS <artiyanprasetya@gmail.com>
2017-12-21 10:03:29 +0700

    halaman tempat futsal

--- app/src/main/java/com/asus/futsalngalam_petugas/HomeFragment.java
+++ app/src/main/java/com/asus/futsalngalam_petugas/HomeFragment.java
@@ -2,14 +2,11 @@ package com.asus.futsalngalam_petugas;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 
 /**
@@ -30,26 +27,6 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         context = rootView.getContext(); // Assign your rootView to context
 
-        Button detail = rootView.findViewById(R.id.detail);
-        CardView tempatFutsal = rootView.findViewById(R.id.tempatFutsal);
-
-        detail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Pass the context and the Activity class you need to open from the Fragment Class, to the Intent
-                Intent intent = new Intent(context, TempatFutsalActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        tempatFutsal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Pass the context and the Activity class you need to open from the Fragment Class, to the Intent
-                Intent intent = new Intent(context, TempatFutsalActivity.class);
-                startActivity(intent);
-            }
-        });
         return rootView;
     }
 }
